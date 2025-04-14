c1e8bfddc277cd956bad87fd34b58f815964538f
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2025-04-15 04:10:48 +0530

    Initial Commit

--- src/main/java/com/gyarsilalsolanki011/expense/SmartExpenseTrackerApplication.java
+++ src/main/java/com/gyarsilalsolanki011/expense/SmartExpenseTrackerApplication.java
@@ -1,5 +1,6 @@
 package com.gyarsilalsolanki011.expense;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartExpenseTrackerApplication {
 
 	public static void main(String[] args) {
+		// Load custom .env file
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")  // or "expense.env" if that’s your file
+				.load();
+
+		// Set them as system properties
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 		SpringApplication.run(SmartExpenseTrackerApplication.class, args);
 	}
 
